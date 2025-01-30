e55791f44483dc0d4a69530703865f5b07a6bf25
SHAHIL440 <110395532+Shahilali5@users.noreply.github.com>
2025-01-30 17:30:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "29893020"
-API_HASH = "28e79037f0b334ef0503466c53f08af5"
+API_ID = "26131042"
+API_HASH = "c3490f11ce698fece6d44e6e8a91f9b8"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", 6399386263))
-SUPPORT_GRP = "Blissfull_Music"
-UPDATE_CHNL = "akaChampu"
-OWNER_USERNAME = "itsMeShivanshu"
+OWNER_ID = int(getenv("OWNER_ID", 8129810243))
+SUPPORT_GRP = "https://t.me/+cwRCe3o1GD5hYWI1"
+UPDATE_CHNL = "UnknownXMusices"
+OWNER_USERNAME = "Shahil440"
